010bbc463a1036e3a2b7eda4d8cc592232b9592b
Joshua Silvagnoli <silvagnolijoshua@gmail.com>
2026-08-14 16:30:37 -0400

    Add reusable media library page functionality

---
+++ library.js
@@ -0,0 +1,49 @@
+async function getData(){
+    const params = new URLSearchParams(window.location.search);
+    const type = params.get("type");
+    console.log(type);
+    const response = await fetch(
+        "http://127.0.0.1:8000/media?media_type="
+        + encodeURIComponent(type)
+    );
+
+    if (response.status === 200){
+        return response.json();
+    }
+}
+
+const popup = document.getElementById("confirmation");
+
+export async function displayData(){
+    const data = await getData();
+
+    const container = document.getElementById("results");
+
+    data.forEach(media => {
+        const card = document.createElement("div");
+
+        const cardData = {
+            id: media.id,
+            title: media.title,
+            description: media.description,
+            releaseDate: media.release_date,
+            rating: media.rating,
+            image: media.image,
+            type: media.type
+        };
+        
+        Object.entries(cardData).forEach(([key, value]) => {
+            card.dataset[key] = value;
+        });
+
+        card.innerHTML = `
+            <h1>${media.title}</h1>
+            <img src="${media.image}">
+            <h2>Release Date: ${media.release_date}</h2>
+            <p>${media.description}</p>
+            <h2>Rating: ${media.rating}</h2>
+        `;
+
+        container.appendChild(card);
+    });     
+}
